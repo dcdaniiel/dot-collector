aabc82aa0254b5079a402d29622beea3074fa917
Daniel Teixeira <dc.daniiel@gmail.com>
2020-09-22 19:59:11 -0300

    user migration

---
+++ knexfile.js
@@ -0,0 +1,42 @@
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'dot-collector',
+      user: 'daniel',
+      password: 'sehl0k',
+    },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};
